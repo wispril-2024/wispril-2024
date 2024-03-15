bcb43705cef472087372923ea57b1769a449bc5e
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-03-15 09:06:37 +0700

    feat: add banner

---
+++ app/graduates/page.tsx
@@ -0,0 +1,15 @@
+"use server";
+
+// TODO: Add theme color
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center py-12">
+      <img src="/graduates/banner.svg" />
+      <p className="font-westmeath text-3xl">
+        Jangan Lupa Kirimkan WisprillFess kepada Temanmu!
+      </p>
+    </div>
+  );
+};
+
+export default Page;
