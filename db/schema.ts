3d70943f035411444cf49b64254b0013154f105a
FarelW <eeksapi321@gmail.com>
2024-03-18 16:54:29 +0700

    feat: user and tafair attributes updated

--- db/schema.ts
+++ db/schema.ts
@@ -1,13 +1,84 @@
 import type { AdapterAccount } from "@auth/core/adapters";
-import { relations } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import {
   timestamp,
   pgTable,
   text,
   primaryKey,
   integer,
+  numeric,
+  pgEnum,
+  json
 } from "drizzle-orm/pg-core";
 import { v4 as uuidv4 } from "uuid";
+
+export const Fakultas = pgEnum('fakultas',[
+  "FITB",
+  "FMIPA",
+  "FSRD",
+  "FTI",
+  "FTMD",
+  "FTTM",
+  "FTSL",
+  "SAPPK",
+  "SBM",
+  "SF",
+  "SITH",
+  "STEI"
+])
+
+export const Jurusan =  pgEnum('jurusan',[
+  "Meteorologi", 
+  "Oseanografi", 
+  "Teknik Geodesi dan Geomatika", 
+  "Teknik Geologi", 
+  "Aktuaria", 
+  "Astronomi", 
+  "Fisika", 
+  "Kimia",
+  "Matematika", 
+  "Desain Interior", 
+  "Desain Komunikasi Visual", 
+  "Desain Produk", 
+  "Kriya", 
+  "Seni Rupa", 
+  "Teknik Dirgantara", 
+  "Teknik Material", 
+  "Teknik Mesin", 
+  "Teknik Geofisika", 
+  "Teknik Metalurgi", 
+  "Teknik Perminyakan", 
+  "Teknik Pertambangan", 
+  "Rekayasa Infrastruktur Lingkungan", 
+  "Teknik dan Pengelolaan Sumber Daya Air", 
+  "Teknik Kelautan", 
+  "Teknik Lingkungan", 
+  "Teknik Sipil", 
+  "Manajemen Rekayasa Industri", 
+  "Teknik Bioenergi dan Kemurgi", 
+  "Teknik Fisika", 
+  "Teknik Industri", 
+  "Teknik Kimia", 
+  "Teknik Pangan", 
+  "Arsitektur", 
+  "Perencanaan Wilayah dan Kota", 
+  "Kewirausahaan", 
+  "Manajemen", 
+  "Farmasi Klinik dan Komunitas", 
+  "Sains dan Teknologi Farmasi", 
+  "Biologi", 
+  "Mikrobiologi", 
+  "Rekayasa Hayati", 
+  "Rekayasa Pertanian", 
+  "Rekayasa Kehutanan", 
+  "Teknologi Pascapanen",
+  "Sistem dan Teknologi Informasi", 
+  "Teknik Biomedis", 
+  "Teknik Elektro", 
+  "Informatika", 
+  "Teknik Telekomunikasi", 
+  "Teknik Tenaga Listrik"
+])
 
 export const users = pgTable("user", {
   id: text("id")
@@ -15,6 +86,10 @@ export const users = pgTable("user", {
     .primaryKey(),
   name: text("name"),
   username: text("username").unique(),
+  password: text("password"),
+  nim: numeric("NIM"),
+  jurusan: Jurusan('jurusan'),
+  fakultas: Fakultas('fakultas'),
   email: text("email").notNull(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
@@ -72,7 +147,7 @@ export const menfess = pgTable("menfess", {
     .primaryKey(),
   content: text("content"),
   createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
-  userId: text("userID"),
+  userId: text("userId"),
 });
 
 export const taFair = pgTable("taFair", {
@@ -82,7 +157,9 @@ export const taFair = pgTable("taFair", {
   userId: text("userId")
     .references(() => users.id)
     .unique(),
-  content: text("content").notNull(),
+  title: text("title"),
+  content: json("content"),
+  link: json("link"),
   likes: integer("likes").default(0),
   createdAt: timestamp("createdAt", { mode: "date" }).defaultNow(),
 });
