0f948e0c57d688d9675990dc85f5d1491cd2d1ee
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-03-19 21:28:13 +0700

    feat: card details portrait mode

--- app/graduates/cardDetails.tsx
+++ app/graduates/cardDetails.tsx
@@ -1,4 +1,5 @@
 import { Graduate } from "./graduatesView";
+import { cn } from "@/lib/utils";
 import { X } from "lucide-react";
 import Image from "next/image";
 
@@ -12,6 +13,8 @@ type CardDetailsProp = {
 };
 
 export function CardDetails({ onClose = () => {}, data }: CardDetailsProp) {
+  let portrait = window.innerHeight > window.innerWidth;
+
   return (
     <div className="fixed left-0 top-0 z-50 flex h-screen w-screen items-center justify-center p-10 text-[#F4D38E]">
       <div
@@ -19,46 +22,82 @@ export function CardDetails({ onClose = () => {}, data }: CardDetailsProp) {
         onClick={onClose}
       ></div>
       <div
-        className="relative z-[51] flex"
-        style={{ aspectRatio: containerRatio }}
+        className={cn("max-w-screen relative z-[51] flex max-h-screen p-5")}
+        style={{ aspectRatio: portrait ? 1 / containerRatio : containerRatio }}
       >
         <Image
-          src={"/graduates/card-details-background.png"}
+          src={`/graduates/card-details-background${
+            portrait ? "-rotated" : ""
+          }.png`}
+          priority={true}
           alt=""
-          width={containerWidth}
-          height={containerHeight}
+          width={containerHeight}
+          height={containerWidth}
         />
-        <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center gap-5 overflow-hidden px-[20%] pb-[13%] pt-[7%]">
+        <div
+          className={cn(
+            "absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center gap-5 overflow-hidden",
+            portrait
+              ? "flex-col px-[20%] py-[40%]"
+              : "flex-row px-[20%] pb-[13%] pt-[7%]"
+          )}
+        >
           <button
             onClick={onClose}
-            className="absolute left-[7%] top-[10%] -translate-x-1/2 -translate-y-1/2"
+            className={cn(
+              "absolute -translate-x-1/2 -translate-y-1/2",
+              portrait ? "left-[15%] top-[10%]" : "left-[10%] top-[15%] "
+            )}
           >
             <X strokeWidth={5} className="text-lg" />
           </button>
           <img
-            className="max-h-full object-scale-down"
+            className={cn(
+              "object-scale-down",
+              portrait ? "max-h-[50%]" : "max-h-full"
+            )}
             src="/graduates/placeholder.png"
           />
-          <div className="flex h-full flex-grow flex-col gap-3 overflow-x-scroll p-1">
-            <div className="font-westmeath">
+          <div
+            className={cn(
+              "flex flex-col gap-3 overflow-x-scroll p-1",
+              portrait ? "w-full" : "h-full"
+            )}
+          >
+            <div
+              className={cn(
+                "flex w-full flex-col font-westmeath",
+                portrait ? "items-center" : ""
+              )}
+            >
               <p className="text-3xl">Biodata Wisudawan</p>
               <p className="text-2xl">{data.name}</p>
               <p>NIM Wisudawan</p>
               <p>{data.program}</p>
             </div>
 
-            <div className="mt-auto flex flex-col gap-2">
+            <div
+              className={cn(
+                "flex w-full flex-col gap-2",
+                portrait ? "items-center" : "mt-auto"
+              )}
+            >
               <input
-                className="rounded-xl border-2 border-[#F4D38E] bg-[#82080A] px-2 py-1 font-westmeath placeholder-[#F4D38E]"
+                className="w-full rounded-xl border-2 border-[#F4D38E] bg-[#82080A] px-2 py-1 font-westmeath placeholder-[#F4D38E]"
                 placeholder="Anonymous"
                 spellCheck={false}
               />
               <input
-                className="rounded-xl border-2 border-[#F4D38E] bg-[#82080A] px-2 py-1 font-westmeath placeholder-[#F4D38E]"
+                className="w-full rounded-xl border-2 border-[#F4D38E] bg-[#82080A] px-2 py-1 font-westmeath placeholder-[#F4D38E]"
                 placeholder="Write menfess to Wisudawan"
                 spellCheck={false}
               />
-              <button className="self-start rounded-xl bg-[#FFDFA4] px-6 py-1 font-westmeath text-[#B87D12]">
+              <button
+                className={cn(
+                  "rounded-xl bg-[#FFDFA4] px-6 py-1 font-westmeath text-[#B87D12]",
+                  portrait ? "" : "self-start"
+                )}
+              >
                 SEND
               </button>
             </div>
