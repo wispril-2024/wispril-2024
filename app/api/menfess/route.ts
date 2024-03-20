4052d19a8639d6467af91f7f5b162222f4798e81
FarelW <eeksapi321@gmail.com>
2024-03-20 21:00:02 +0700

    fix:  menfess endpoint zod validation

--- app/api/menfess/route.ts
+++ app/api/menfess/route.ts
@@ -1,6 +1,48 @@
-import { NextResponse } from "next/server";
+import { menfess } from "@/db/schema";
+import db from "@/lib/db";
+import { menfessSchema } from "@/lib/zod";
+import { NextRequest, NextResponse } from "next/server";
 
-// GET request
-export const GET = () => {
-  return NextResponse.json({ message: "sukses" }, { status: 200 });
+export const POST = async (req: NextRequest) => {
+  const formData = await req.formData();
+  const userId = formData.get("userId");
+  const content = formData.get("content");
+
+
+  const zodResult = menfessSchema.safeParse({ userId, content });
+
+  if (!zodResult.success) {
+    return NextResponse.json(
+      {
+        error: "Bad Request",
+        message: zodResult.error.issues[0].message,
+      },
+      { status: 400 }
+    );
+  }
+
+  const result = zodResult.data
+
+  try {
+    await db.insert(menfess).values({
+      userId:result.userId,
+      content:result.content
+    });
+
+    return NextResponse.json(
+      {
+        message : "Menfess succesfully created"
+      },
+      { status: 200 }
+    );
+
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: "Internal Server Error",
+        message: "Failed to create menfess",
+      },
+      { status: 500 }
+    );
+  }
 };
