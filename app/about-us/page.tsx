8f62d1ac3edc04321201869b08ac43533222c841
AlbertChoe <albert.choe73@gmail.com>
2024-03-22 11:29:24 +0700

    add data and fix page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,32 +1,20 @@
-import pageBG from "../../public/about-us/bg.png";
 import Description from "./description";
 import OrganogramPage from "./organogram";
-import Title from "./title";
 import VisiMisi from "./visimisi";
-import Image from "next/image";
 
-// export const metadata = {
-//   title: "About Us",
-//   openGraph: {
-//     title: "About Us",
-//   },
-//   twitter: {
-//     title: "About Us",
-//   },
-// };
+export const metadata = {
+  title: "About Us",
+  openGraph: {
+    title: "About Us",
+  },
+  twitter: {
+    title: "About Us",
+  },
+};
 
 export default function Page() {
   return (
-    <main className="relative min-h-screen overflow-hidden  pt-16 ">
-      <Image
-        className="absolute inset-0 -z-50 h-full w-full object-cover object-top"
-        src={pageBG}
-        alt="Background Image"
-        fill={true}
-        sizes="100vw"
-        priority
-      />
-      <Title />
+    <main className="relative min-h-screen overflow-hidden bg-custom-gradient px-1 py-12 sm:py-16 lg:py-24 ">
       <Description />
       <VisiMisi />
       <OrganogramPage />
