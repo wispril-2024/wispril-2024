1e271c98566787c00a184451d49af7d7d0ff0902
Lint Action <lint-action@samuelmeuli.com>
2024-03-23 21:22:37 +0000

    Fix code style issues with Prettier

--- app/api/auth/[...nextauth]/auth-options.ts
+++ app/api/auth/[...nextauth]/auth-options.ts
@@ -1,11 +1,11 @@
-import { eq } from "drizzle-orm";
 import { db } from "../../../../db/drizzle";
+import { users } from "@/db/schema";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { compare } from "bcrypt";
+import { eq } from "drizzle-orm";
 import type { NextAuthOptions, AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import "server-only";
-import { users } from "@/db/schema";
-import { compare } from "bcrypt";
 
 type Adapter = NextAuthOptions["adapter"];
 
@@ -17,60 +17,63 @@ export const authOptions: AuthOptions = {
       name: "Credentials",
       credentials: {
         username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        if(!credentials?.username || !credentials.password){
+        if (!credentials?.username || !credentials.password) {
           console.log("Missing credentials");
           return null;
         }
-        
+
         const existingUser = await db.query.users.findFirst({
-          where: eq(users.username,credentials.username)
+          where: eq(users.username, credentials.username),
         });
 
         if (!existingUser || !existingUser.password) {
-            console.log(`User not found for username: ${credentials.username}`);
-            return null;
+          console.log(`User not found for username: ${credentials.username}`);
+          return null;
         }
 
-        const passwordCheck = await compare(credentials.password, existingUser.password);
+        const passwordCheck = await compare(
+          credentials.password,
+          existingUser.password
+        );
 
         if (!passwordCheck) {
           console.log("Password check failed.");
-            return null;
+          return null;
         }
 
         return {
-            id: `${existingUser.id}`,
-            name: existingUser.username || "",
+          id: `${existingUser.id}`,
+          name: existingUser.username || "",
         };
-      }
-    })
+      },
+    }),
   ],
   session: {
     strategy: "jwt",
     maxAge: 86400,
   },
   callbacks: {
-    async jwt({token,user}) {
-        if (user) {
-            return {
-                ...token,
-                name: user.name
-            }
-        }
-        return token
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          name: user.name,
+        };
+      }
+      return token;
     },
-    async session({session,token}) {
-        return{
-            ...session,
-            user: {
-                ...session.user,
-                name:token.name
-            }
-        }
-    }
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          name: token.name,
+        },
+      };
+    },
   },
   pages: {
     signIn: "/auth/sign-in",
