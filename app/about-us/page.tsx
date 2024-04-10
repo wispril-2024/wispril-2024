594dae41ae13febd4152afc39ddc0c2a7c3e287f
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-04-11 06:39:23 +0700

    fix: theater image posititon.

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -29,18 +29,14 @@ export default function Page() {
       <div className="absolute -right-20 top-0 z-0 aspect-[4/3] h-auto w-64 rounded-full bg-[#A01B14] bg-opacity-60 blur-[80px] lg:w-96 lg:blur-[160px]" />
 
       {/* Background theater decoration */}
-      <div className="absolute -bottom-60 left-0 right-0 z-0 w-full opacity-50">
-        {/* Image mask */}
-        <div className="mx-auto h-[1000px] w-full lg:h-[2100px] lg:w-[1000px]">
-          {/* Theater */}
-          <Image
-            className="h-full w-full object-cover"
-            src="/about-us/teater.png"
-            alt="Background Image"
-            fill={true}
-            sizes="100vw"
-          />
-        </div>
+      <div className="absolute -bottom-24 left-0 right-0 z-0 h-[1000px] w-full opacity-50 lg:h-[2100px]">
+        <Image
+          className="h-full w-full object-cover"
+          src="/about-us/teater.png"
+          alt="Background Image"
+          fill={true}
+          sizes="100vw"
+        />
       </div>
     </main>
   );
