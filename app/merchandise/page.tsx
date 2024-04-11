bf8e6c937845f9e6b58c33a5475b5ab9c468f052
SandWithCheese <naufalahmad022@gmail.com>
2024-04-11 12:40:31 +0700

    refactor: refactor folder name to use lowercase

---
+++ app/merchandise/page.tsx
@@ -0,0 +1,89 @@
+import Title from "./title";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const bgImageSrc = "/merchandise/Bg.png";
+const MobilebgImageSrc = "/merchandise/BgMobile.png";
+
+interface Item {
+  image: string;
+  price: string;
+  name: string;
+}
+
+const items: Item[] = [
+  { image: "/merchandise/Merch.png", price: "$10", name: "Item 1" },
+  { image: "/merchandise/Merch.png", price: "$20", name: "Item 2" },
+  { image: "/merchandise/Merch.png", price: "$20", name: "Item 3" },
+  { image: "/merchandise/Merch.png", price: "$20", name: "Item 4" },
+  { image: "/merchandise/Merch.png", price: "$20", name: "Item 5" },
+  { image: "/merchandise/Merch.png", price: "$20", name: "Item 6" },
+];
+
+function ItemsList() {
+  return <div>Hai</div>;
+}
+
+// const ItemsList = () => {
+//   return (
+//     <main className="relative flex min-h-screen flex-col items-center justify-center bg-[#510007]">
+//       <div className="h-full w-full object-cover">
+//         <Image
+//           src={MobilebgImageSrc}
+//           alt="Background"
+//           quality={100}
+//           className="mt-2 object-center sm:hidden md:hidden lg:hidden xl:mt-0 xl:hidden"
+//           layout="fill"
+//         />
+//         <Image
+//           src={bgImageSrc}
+//           alt="Background"
+//           quality={100}
+//           className="mt-2 hidden object-center opacity-80 sm:block md:block lg:block xl:block"
+//           layout="fill"
+//         />
+//       </div>
+//       <div className="z-20 pb-32 pt-0 sm:pb-0 lg:pt-20">
+//         <Title />
+//       </div>
+//       <div className="z-10 p-10">
+//         <div className="grid grid-cols-1 gap-2.5 sm:grid-cols-2 lg:mt-60 lg:gap-24">
+//           {items.map((item, index) => (
+//             <div key={index} className="mb-5 flex flex-col items-center">
+//               <div className="w-full sm:w-48 md:w-72 xl:w-96">
+//                 <Image
+//                   src={item.image}
+//                   alt={item.name}
+//                   width={198}
+//                   height={265}
+//                   layout="responsive"
+//                 />
+//               </div>
+//               <h2
+//                 style={{ color: "rgba(244, 211, 142, 1)" }}
+//                 className="font-westmeath text-3xl font-normal lg:text-4xl"
+//               >
+//                 Price: {item.price}
+//               </h2>
+//               <h3 className="font-westmeath text-3xl text-white lg:text-4xl ">
+//                 Name: {item.name}
+//               </h3>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//       <Link href="https://docs.google.com/forms/your-form-id" legacyBehavior>
+//         <a
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           className="z-10 mb-10 rounded-xl border-[4px] border-[#B87D12] bg-[#FFDFA4] px-5 py-1.5 font-westmeath text-sm text-white lg:rounded-2xl xl:rounded-2xl xl:px-10 xl:py-5 xl:text-4xl"
+//         >
+//           <p className="text-[#B87D12]">Order Now</p>
+//         </a>
+//       </Link>
+//     </main>
+//   );
+// };
+
+export default ItemsList;
--- app/merchandise/title.tsx
+++ app/merchandise/title.tsx
@@ -1,4 +1,3 @@
-import lampu from "../../public/Merchandise/Lampu.png";
 import Image from "next/image";
 
 export default function Title() {
@@ -6,18 +5,18 @@ export default function Title() {
     <main className="z-10 mt-10 md:mt-20 lg:mt-2 xl:mt-10">
       <div className=" flex flex-row items-center justify-center">
         <Image
-          src={lampu}
+          src="/merchandise/Lampu.png"
           alt="lampu"
           width={100}
           height={100}
-          className=" drop-shadow-[0px_0px_20px_#F4D38E]"
+          className="drop-shadow-[0px_0px_20px_#F4D38E]"
         />
         <h1 className="bg-gradient-to-r from-[#F4D38E] to-[#EAC050] bg-clip-text font-westmeath text-[46px] font-normal text-transparent shadow-[#F4D38E] [text-shadow:2px_2px_10px_var(--tw-shadow-color)]  md:text-8xl  ">
           Merchandise
         </h1>
 
         <Image
-          src={lampu}
+          src="/merchandise/Lampu.png"
           alt="lampu"
           width={100}
           height={100}
