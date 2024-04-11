87acb209f9e843e48ea1b2b31611d0f29e94fa57
Dewantoro Triatmojo <dewantorotriatmojo@gmail.com>
2024-04-11 23:18:35 +0700

    fix: menfess card

--- app/dashboard/inbox/menfess-card.tsx
+++ app/dashboard/inbox/menfess-card.tsx
@@ -1,3 +1,4 @@
+import { getFormattedDate } from "@/lib/utils";
 import type { MenfessMessage } from "@/types/menfess";
 import Image from "next/image";
 
@@ -5,24 +6,36 @@ type MenfessCardProps = MenfessMessage;
 
 const MenfessCard = ({ from, message, date }: MenfessCardProps) => {
   return (
-    <div className="z-10 mt-5 flex w-2/3 flex-row gap-2.5 rounded-[0.5rem] border-2 border-[#F4D38E] bg-[#82080A] p-6">
-      <div>
+    <article className="flex w-full max-w-4xl flex-col gap-2 rounded-lg border-2 border-[#F4D38E] bg-[#82080A] p-4 lg:gap-3 lg:p-5">
+      <div className="flex flex-row items-center gap-2 lg:gap-3">
+        {/* Avatar */}
         <Image
           src="/dashboard/default-avatar.png"
           alt="Default Avatar"
-          className="rounded-full border-2 border-[#F4D38E] object-cover"
-          width={60}
-          height={60}
-          style={{ aspectRatio: "1 / 1" }}
+          className="aspect-square size-12 rounded-full border-2 border-[#F4D38E] object-cover object-center lg:size-14"
+          width={40}
+          height={40}
         />
+
+        {/* Message Info */}
+        <div className="flex w-full flex-col">
+          {/* From */}
+          <p className="line-clamp-1 font-cgp text-base font-bold text-[#F4D38E] lg:text-lg">
+            {from}
+          </p>
+
+          {/* Date */}
+          <p className="font-cgp text-xs font-semibold text-[#F4D38E] lg:text-sm">
+            {getFormattedDate(date)}
+          </p>
+        </div>
       </div>
-      <div className="flex w-full flex-col gap-2.5">
-        <p className="font-cgp font-bold text-[#F4D38E]">{from}</p>
-        <p className="text-justify font-cgp font-normal text-[#F4D38E]">
-          {message}
-        </p>
-      </div>
-    </div>
+
+      {/* Message Content */}
+      <p className="font-cgp text-sm font-normal text-[#F4D38E] lg:text-base">
+        {message}
+      </p>
+    </article>
   );
 };
 
