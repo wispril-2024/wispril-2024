a904f3ed89b08ff90d79be1b1426e0ff0b5f955c
SandWithCheese <naufalahmad022@gmail.com>
2024-04-17 13:11:14 +0700

    fix: change timeline size

--- app/(home)/timeline.tsx
+++ app/(home)/timeline.tsx
@@ -6,7 +6,7 @@ export default function Timeline() {
       <h1 className="z-10 bg-gradient-to-r from-[#F4D38E] to-[#EAC050] bg-clip-text text-center font-westmeath text-3xl font-normal text-transparent sm:text-4xl md:text-5xl xl:text-7xl">
         TIMELINE
       </h1>
-      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden p-20 sm:p-32 md:p-48">
+      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden p-14 sm:p-32 md:p-48">
         <Image
           src="/home/ticket.png"
           alt="Background Ticket"
@@ -21,7 +21,7 @@ export default function Timeline() {
             alt="Timeline"
             width={1920}
             height={1080}
-            className="z-10 w-[1000px]"
+            className="z-10 w-[800px]"
           />
         </div>
       </div>
